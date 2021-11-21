3ad22329dfa68bc01ce532476625794807fbdca4
Aiden Bai <aiden.bai05@gmail.com>
2021-11-21 13:21:02 -0800

    revert to 20517870180e656a6a9692273c56a5c9170d76f9

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = require('nextra')({
+const nextra = require('nextra')({
   theme: 'nextra-theme-blog',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
 })();
+
+module.exports = {
+  ...nextra,
+  swcMinify: true,
+};
