5819bde0afb0fa21958224aadf6d2af6af1ca251
Aiden Bai <aiden.bai05@gmail.com>
2021-12-28 14:21:57 -0800

    minor adj

--- components/GenericName.js
+++ components/GenericName.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
-const parts = [
+const prefix = ['A+', '4.0', 'Ivy', 'SAT', 'ACT', '1600', 'PSAT', 'AP', 'Star'];
+
+const suffix = [
   'Youth',
   'Science',
   'Research',
@@ -20,10 +22,29 @@ const parts = [
   'World',
   'Technology',
   'Young',
+  'Engineering',
+  'Schoolhouse',
+  'School',
+  'Teacher',
+  'Excel',
+  'Hicap',
+  'Honors',
+  'Brain',
+  'Best',
+  'Class',
+  'College',
+  'Prep',
+  'Expert',
+  'Academy',
+  'Accelerator',
+  'IQ',
+  'Intelligent',
+  'Einstein',
 ];
 
-const getWord = () => parts[Math.floor(Math.random() * parts.length)];
-const createName = () => `${getWord()} ${getWord()} ${getWord()} ${getWord()}`;
+const getPrefix = () => prefix[Math.floor(Math.random() * prefix.length)];
+const getSuffix = () => suffix[Math.floor(Math.random() * suffix.length)];
+const createName = () => `${getPrefix()} ${getSuffix()} ${getSuffix()} ${getSuffix()}`;
 
 export default function GenericName() {
   const [name, setName] = useState(createName());
@@ -33,8 +54,11 @@ export default function GenericName() {
         style={{
           padding: 5,
           marginRight: 15,
-          background: '#a0afc054',
-          borderRadius: 5,
+          background: '#334155',
+          color: '#fff',
+          borderRadius: 10,
+          paddingLeft: 20,
+          paddingRight: 20,
         }}
         onClick={() => setName(createName())}
       >
--- components/Hash.js
+++ components/Hash.js
@@ -9,19 +9,28 @@ export default function Hash() {
         className="border-red"
         ref={inputRef}
         placeholder="Paste hash"
-        style={{ padding: 5, marginRight: 15, border: '1px solid #a0afc0', borderRadius: 5 }}
+        style={{
+          padding: 5,
+          marginRight: 15,
+          border: '1px solid #a0afc0',
+          borderRadius: 5,
+          fontFamily:
+            'SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
+        }}
         onChange={() => {
           try {
             console.log(JSON.parse(atob(inputRef.current.value)));
             setDecoded(JSON.parse(atob(inputRef.current.value)));
-          } catch (_err) {}
+          } catch (_err) {
+            setDecoded({});
+          }
         }}
       />
       {decoded.name && decoded.hours && decoded.time ? (
-        <span>
-          Name: <code>{decoded.name}</code>, Hours: <code>{decoded.hours}</code>, Time:{' '}
-          <code>{new Date(decoded.time).toLocaleDateString()}</code>
-        </span>
+        <>
+          <code>{decoded.name}</code>, <code>{new Date(decoded.time).toLocaleDateString()}</code>,{' '}
+          Hours: <code>{decoded.hours}</code>
+        </>
       ) : (
         <code>Invalid Hash</code>
       )}
